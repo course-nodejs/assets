bd7e8b4db249d77264a3838491547e9af506b80c
Fabio Sfuncia <fsfuncia@unime.it>
2017-12-04 15:03:48 +0100

    some stuff

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ module.exports = {
  // crea/svuota collection
  init: function(callback){
    MongoClient.connect(config.db.dsn, config.db.options, function (err, db) {
-    if (err) return trow err
+    if (err) throw err
     //creo/svuoto la collection
     db.createCollection(config.db.collection_name, function (err) {
-      if (err) return trow err
+      if (err) throw err
       db.collection(config.db.collection_name).drop(function (err, dropped) {
-        if (err) trow err
-		callback()
+        if (err) throw err
+        db.close()
+		callback(err)
       })
     })
    })
@@ -30,25 +31,25 @@ module.exports = {
      })
    })
  },
- updateState: function(uuid, state){
+ updateState: function(uuid, state, callback){
    MongoClient.connect(config.db.dsn, config.db.options, function (err, db) {
-     if (err) return trow err
+     if (err) throw err
      //update
      db.collection(config.db.collection_name).updateOne({uuid: uuid}, {state:state}, function (err, res) { 
-       if (err) trow err
-       console.log(res)
+       if (err) throw err
        db.close()
+       callback(err, res)
      })
    })
  },
- queryState: function(uuid){
+ queryState: function(uuid, callback){
    MongoClient.connect(config.db.dsn, config.db.options, function (err, db) {
-     if (err) return trow err
+     if (err) throw err
      //query
      db.collection(config.db.collection_name).find({uuid: uuid}).toArray(function (err, res) { 
-       if (err) trow err
-       console.log(res)
+       if (err) throw err
        db.close()
+       callback(err,res[0].state)
      })
    })
  }
---
+++ test/query.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const config = require('../config/default.js')
+const MongoClient = require('mongodb').MongoClient
+const asset_service = require('../index.js')
+const tap = require('tap')
+
+asset_service.init(function() {
+  tap.test('test query', function(childTest) {
+    asset_service.insert({ name:'Sedia',state:'wait'}, function(err, res) {
+      if (err) throw err
+      asset_service.queryState(res.uuid, function(err, res) {
+        if (err) throw err
+        console.log(res)
+        childTest.equal(res,'wait')
+        childTest.end()
+      })
+    })
+  })
+})
